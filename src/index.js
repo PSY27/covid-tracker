ce5b27cc8ad858d6ed1b99d16b255b2a33ce5cac
SAI SRAVAN MEDICHERLA <saisraone2002@gmail.com>
2022-04-05 01:05:47 +0530

    removed firebase

--- src/index.js
+++ src/index.js
@@ -3,21 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { initializeApp } from 'firebase/app';
 
-// TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCEPtTaKEZzE6tiWR-apuMq21x-oy-pT0g",
-  authDomain: "covid-19-tracker-48d29.firebaseapp.com",
-  projectId: "covid-19-tracker-48d29",
-  storageBucket: "covid-19-tracker-48d29.appspot.com",
-  messagingSenderId: "195874009940",
-  appId: "1:195874009940:web:15fb94d3aca3c9ee548ec1",
-  measurementId: "G-43GPFYZTMC"
-};
-
-// eslint-disable-next-line no-unused-vars
-const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
